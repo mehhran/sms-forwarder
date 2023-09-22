3ad14810b59165eb538f5679c9c125c81d484fa6
meh jav <mehhran@gmail.com>
2023-09-21 17:11:52 -0700

    restarter

---
+++ app/src/main/java/cloud/mehran/smsforwarder/ServiceRestarterBroadcastReceiver.java
@@ -0,0 +1,15 @@
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import androidx.core.content.ContextCompat;
+
+public class ServiceRestarterBroadcastReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Intent serviceIntent = new Intent(context, ForegroundService.class);
+        serviceIntent.putExtra("inputExtra", "Listening for new messages...");
+        ContextCompat.startForegroundService(context, serviceIntent);
+    }
+}
